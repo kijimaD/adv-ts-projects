a26ad41074c729f67a10c7d824de084a55c2f2c2
kijima <norimaking777@gmail.com>
2021-09-04 00:32:48 +0900

    Add(not working)

--- chapter03/src/App.tsx
+++ chapter03/src/App.tsx
@@ -1,37 +1,31 @@
 import * as React from 'react';
-import './App.css';
 import Container from 'reactstrap/lib/Container';
+import './App.css';
 import PersonalDetails from './PersonalDetails';
+import { IPersonState } from "./State";
 
 export default class App extends React.Component {
+  private defaultPerson : IPersonState = {
+    Address1: "",
+    Address2: null,
+    County: "",
+    DateOfBirth : new Date().toISOString().substring(0, 10),
+    FirstName: "",
+    LastName: "",
+    PersonId : "",
+    PhoneNumber: "",
+    Postcode: "",
+    Town: ""
+  }
+  constructor(props : {}) {
+    super(props);
+  }
+
   public render() {
     return (
       <Container>
-        <PersonalDetails />
+        <PersonalDetails DefaultState={this.defaultPerson}/>
       </Container>
     );
   }
 }
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.tsx</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;

6b8db33d03436872e2f6bb8d2bef9faf9594c065
kijima <norimaking777@gmail.com>
2021-09-04 21:45:55 +0900

    Add button

--- trello-clone/src/NewItemForm.tsx
+++ trello-clone/src/NewItemForm.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react"
 import { NewItemFormContainer, NewItemButton, NewItemInput } from "./styles"
+import { useFocus } from "./utils/useFocus"
 
 interface NewItemFormProps {
   onAdd(text: string): void
@@ -7,16 +8,16 @@ interface NewItemFormProps {
 
 export const NewItemForm = ({ onAdd }: NewItemFormProps) => {
   const [text, setText] = useState("")
+  const inputRef = useFocus()
 
   return (
     <NewItemFormContainer>
       <NewItemInput
+        ref={inputRef}
         value={text}
         onChange={e => setText(e.target.value)}
       />
-      <NewItemButton onClick={() => onAdd(text)}>
-        Create
-      </NewItemButton>
+      <NewItemButton onClick={() => onAdd(text)}>Create</NewItemButton>
     </NewItemFormContainer>
   )
 }
